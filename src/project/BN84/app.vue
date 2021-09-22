b52f010642a32f11637e6162f7db655ce6bdc5a4
ov <ov@smie.one>
2021-09-22 13:26:13 +0800

    add BN84

---
+++ src/project/BN84/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <Main></Main>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Main from '@/layout/Main'
+
+export default {
+  name: 'BN84',
+  components: {
+    Main
+  },
+  data(){
+    return{
+      gameToken: '2f68afc8f3974c5c84b5e7c61486076d', // 本地端開發token
+      multiple: 0.5 // 押注乘數基本分
+    }
+  },
+  computed: {
+    ...mapState({
+      openMode: state => state.openMode,
+    }),
+  },
+  created(){
+    /** 專案初始化設定 */
+    if(this.openMode === 'game' && process.env.NODE_ENV === 'development'){
+      // 遊戲內部開啟細單
+      this.$store.commit('APP_TOKEN', this.gameToken);
+    }
+    this.$store.commit('SET_MULTIPLE', this.multiple);
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
+
---
+++ src/project/BN84/spinDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="spinDetails">
+    <h2>{{ $t('GameScreen') }}</h2>
+    <section v-for="(list,idx) in renderData" :key="idx">
+      <screen-info :infos="list.infos" :screens="list.screens"></screen-info>
+      <line-info :lines="list.lines" :screens="list.screens" ></line-info>
+    </section>
+  </div>
+</template>
+
+<script>
+import slot from '@/mixin/slot'
+
+export default {
+  name: 'spinDetails',
+  mixins: [slot], // Slot預設基本函式(客製化時可使用相同函式名稱覆蓋)
+}
+</script>
+
+<style lang="scss">
+@import '@css/slotSpin.scss';
+</style>
